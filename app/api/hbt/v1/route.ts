4a7d5609d97962c3b3dd5d3fd7f6168972e477e3
saicharanpogul <pogul1804@gmail.com>
2024-02-27 00:30:33 +0530

    Added HBT Route

---
+++ app/api/hbt/v1/route.ts
@@ -0,0 +1,64 @@
+import { HBTBack, HBTFront } from "@/app/utils";
+import _ from "lodash";
+
+const sendFileResponse = async (
+  filetype: string | string[],
+  png: Buffer,
+  svg: any
+) => {
+  if (filetype === "png") {
+    return new Response(png, { headers: { "Content-Type": "image/png" } });
+  } else if (filetype === "svg") {
+    return new Response(svg, { headers: { "Content-Type": "image/svg+xml" } });
+  } else {
+    return Response.json(
+      {
+        message: "Invalid filetype.",
+      },
+      { status: 400 }
+    );
+  }
+};
+
+const paramMissing = async (param: string | string[], name: string) => {
+  if (!param) {
+    console.log(param, name);
+    return Response.json(
+      {
+        message: `${_.capitalize(name)} is Missing.`,
+      },
+      { status: 400 }
+    );
+  }
+};
+
+export async function GET(req: Request) {
+  const { searchParams } = new URL(req.url);
+  const { calories, address, datetime, filetype } = Object.fromEntries(
+    searchParams.entries()
+  );
+
+  await paramMissing(
+    datetime as string,
+    Object.keys({ datetime }).pop()!.toString()
+  );
+  await paramMissing(
+    filetype as string,
+    Object.keys({ filetype }).pop()!.toString()
+  );
+
+  await paramMissing(
+    calories as string,
+    Object.keys({ calories }).pop()!.toString()
+  );
+  await paramMissing(
+    address as string,
+    Object.keys({ address }).pop()!.toString()
+  );
+  const { svg, png } = await HBTBack(
+    String(calories),
+    String(address),
+    Number(datetime)
+  );
+  return await sendFileResponse(filetype as string, png, svg);
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,41 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        primary: {
+          main: "#43E0B3",
+          light: "#000000",
+          dark: "#1C4942",
+        },
+        background: {
+          main: "#222222",
+          light: "#2B2B2B",
+          dark: "#000000",
+        },
+        error: {
+          main: "#EA4F30",
+          light: "#FAEDEA",
+          dark: "#160705",
+        },
+        warning: {
+          main: "#F0AD2D",
+          light: "#FDF4E7",
+          dark: "#170F02",
+        },
+        success: {
+          main: "#1EB871",
+          light: "#EFF7EE",
+          dark: "#091108",
+        },
+        text: {
+          main: "#F7FAF3",
+          light: "#B0B0B0",
+          dark: "#000000",
+        },
+        transparent: {
+          main: "rgba(0, 0, 0, 0.5)",
+          dark: "rgba(0, 0, 0, 0.7)",
+        },
       },
     },
   },
