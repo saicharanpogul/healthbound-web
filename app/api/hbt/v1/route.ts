2bb8c143235b92a043da85317015e5393302a490
saicharanpogul <pogul1804@gmail.com>
2024-02-27 15:34:53 +0530

    Removed Sharp

--- app/api/hbt/v1/route.ts
+++ app/api/hbt/v1/route.ts
@@ -1,24 +1,5 @@
-import { HBTBack, HBTFront } from "@/app/utils";
+import fs from "fs";
 import _ from "lodash";
-
-const sendFileResponse = async (
-  filetype: string | string[],
-  png: Buffer,
-  svg: any
-) => {
-  if (filetype === "png") {
-    return new Response(png, { headers: { "Content-Type": "image/webp" } });
-  } else if (filetype === "svg") {
-    return new Response(svg, { headers: { "Content-Type": "image/svg+xml" } });
-  } else {
-    return Response.json(
-      {
-        message: "Invalid filetype.",
-      },
-      { status: 400 }
-    );
-  }
-};
 
 const paramMissing = async (param: string | string[], name: string) => {
   if (!param) {
@@ -34,7 +15,7 @@ const paramMissing = async (param: string | string[], name: string) => {
 
 export async function GET(req: Request) {
   const { searchParams } = new URL(req.url);
-  const { calories, address, datetime, filetype } = Object.fromEntries(
+  const { data, address, datetime } = Object.fromEntries(
     searchParams.entries()
   );
 
@@ -42,23 +23,26 @@ export async function GET(req: Request) {
     datetime as string,
     Object.keys({ datetime }).pop()!.toString()
   );
-  await paramMissing(
-    filetype as string,
-    Object.keys({ filetype }).pop()!.toString()
-  );
 
-  await paramMissing(
-    calories as string,
-    Object.keys({ calories }).pop()!.toString()
-  );
+  await paramMissing(data as string, Object.keys({ data }).pop()!.toString());
   await paramMissing(
     address as string,
     Object.keys({ address }).pop()!.toString()
   );
-  const { svg, png } = await HBTBack(
-    String(calories),
-    String(address),
-    Number(datetime)
-  );
-  return await sendFileResponse(filetype as string, png, svg);
+
+  const _date = new Date(Number(datetime));
+  const formattedDate = `${_date.getDate()}/${_date.getMonth()}/${_date.getFullYear()}`;
+
+  const filePath = "./public/HBT.svg";
+  const image = fs.readFileSync(filePath, "utf-8");
+
+  let svg = image;
+
+  svg = svg.replace("{address}", address);
+  svg = svg.replace("{data}", data);
+  svg = svg.replace("{date}", formattedDate);
+
+  return new Response(svg, {
+    headers: { "Content-Type": "image/svg+xml" },
+  });
 }
