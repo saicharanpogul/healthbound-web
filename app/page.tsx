ad4626e3ad89dd7f933df11073430ba1823f2dd0
saicharanpogul <pogul1804@gmail.com>
2024-02-29 14:05:12 +0530

    Added Subtitle

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,10 @@ export default function Home() {
       <p className="text-3xl md:text-6xl text-primary-main select-none">
         HEALTHBOUND
       </p>
+      <p className="mt-4 max-w-[96%] md:max-w-[50%] text-center text-lg text-text-main opacity-80">
+        Create your soulbound token associated with your health & claim
+        Healthbound Tokens every day
+      </p>
     </main>
   );
 }
